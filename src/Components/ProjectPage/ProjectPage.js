4ccc9ae23269b28192089d33761df3d3ddd7667f
kathrynzaro <zarokathryn@gmail.com>
2022-09-19 17:03:15 -0700

    flex edits

--- src/Components/ProjectPage/ProjectPage.js
+++ src/Components/ProjectPage/ProjectPage.js
@@ -42,14 +42,16 @@ export default function ProjectPage() {
               pop-ups in your VS Code window.
             </p>
           </div>
-          <div className="video-mask">
-            <video className="boobot-vid" autoPlay loop muted>
-              <source src="/booghost.mp4" type="video/mp4"></source>
-            </video>
+          <div className="media">
+            <div className="video-mask">
+              <video className="boobot-vid" autoPlay loop muted>
+                <source src="/booghost.mp4" type="video/mp4"></source>
+              </video>
+            </div>
+            <a href="#printstagram">
+              <img className="arrow" src="/arrowdown.png" />
+            </a>
           </div>
-          <a href="#printstagram">
-            <img className="arrow" src="/arrowdown.png" />
-          </a>
         </div>
         <div className="project-card" id="printstagram">
           <div className="content">
@@ -76,18 +78,20 @@ export default function ProjectPage() {
               for animals that are currently up for adoption.
             </p>
           </div>
-          <div className="video-mask">
-            <video className="printstagram-vid" autoPlay loop muted>
-              <source src="/prinsta.mp4" type="video/mp4"></source>
-            </video>
-          </div>
-          <div className="arrows">
-            <a href="#boobot">
-              <img className="arrow" src="/arrowup.png" />
-            </a>
-            <a href="#vsssnake">
-              <img className="arrow" src="/arrowdown.png" />
-            </a>
+          <div className="media">
+            <div className="video-mask">
+              <video className="printstagram-vid" autoPlay loop muted>
+                <source src="/prinsta.mp4" type="video/mp4"></source>
+              </video>
+            </div>
+            <div className="arrows">
+              <a href="#boobot">
+                <img className="arrow" src="/arrowup.png" />
+              </a>
+              <a href="#vsssnake">
+                <img className="arrow" src="/arrowdown.png" />
+              </a>
+            </div>
           </div>
         </div>
         <div className="project-card" id="vsssnake">
@@ -116,10 +120,12 @@ export default function ProjectPage() {
             </p>
           </div>
 
-          <img src="/snake.png"></img>
-          <a href="#printstagram">
-            <img className="arrow" src="/arrowup.png" />
-          </a>
+          <div className="media">
+            <img src="/snake.png"></img>
+            <a href="#printstagram">
+              <img className="arrow" src="/arrowup.png" />
+            </a>
+          </div>
         </div>
       </div>
       {/* <footer><a href="/connect">Connect</a></footer> */}
