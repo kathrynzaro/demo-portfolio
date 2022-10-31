77d973884d27dfb802a58ca6fdda285f34d9f321
kathrynzaro <zarokathryn@gmail.com>
2022-10-31 11:28:00 -0700

    adds stellarview and updated resume

--- src/Components/ProjectPage/ProjectPage.js
+++ src/Components/ProjectPage/ProjectPage.js
@@ -5,6 +5,52 @@ export default function ProjectPage() {
   return (
     <div className="App">
       <div className="project-page">
+        <div className="project-card" id="stellarview">
+          <div className="content">
+            <h1>stellarView</h1>
+            <a
+              href="https://github.com/stellarview"
+              target="_blank"
+              rel="noreferrer"
+              className="project-links"
+            >
+              GitHub
+            </a>
+            <a
+              href="https://stellarview.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+              className="project-links"
+            >
+              Live
+            </a>
+
+            <h3>React, Node.js, Express, PostgreSQL, Jest.</h3>
+            <p>
+              Earthlings, may I introduce stellarView, a full-stack space-themed quiz app
+              that&apos;s on a mission to assist software engineers as they build confidence for
+              their next technical interview by providing 100+ questions across seven categories
+              drawn from stellarView&apos;s RESTful API server. It&apos;s a lot like flashcards,
+              only it&apos;s more fun and it has more aliens and even a little space confetti.
+            </p>
+          </div>
+          <div className="media">
+            <div className="video-mask">
+              <video className="stellarview-vid" playsinline autoPlay loop muted>
+                <source src="/stellarview.mp4" type="video/mp4"></source>
+              </video>
+            </div>
+            <div className="arrows">
+              <a href="#top">
+                <img className="arrow" src="/arrowup.png" />
+              </a>
+              <a href="#boobot">
+                <img className="arrow" src="/arrowdown.png" />
+              </a>
+            </div>
+          </div>
+        </div>
+
         <div className="project-card" id="boobot">
           <div className="content">
             <h1>boo!BOT</h1>
@@ -32,7 +78,7 @@ export default function ProjectPage() {
             >
               Extension
             </a>
-            <h3>Node.js, Express, PostgreSQL, CSS, Jest, Heroku.</h3>
+            <h3>Node.js, Express, PostgreSQL, Jest.</h3>
             <p>
               boo!BOT, your haunted enCounter, is a VS Code extension that was designed for those
               among us who are perpetually waiting for summer to come to an end and cold misty days
@@ -49,12 +95,16 @@ export default function ProjectPage() {
               </video>
             </div>
             <div className="arrows">
+              <a href="#stellarview">
+                <img className="arrow" src="/arrowup.png" />
+              </a>
               <a href="#printstagram">
                 <img className="arrow" src="/arrowdown.png" />
               </a>
             </div>
           </div>
         </div>
+
         <div className="project-card" id="printstagram">
           <div className="content">
             <h1>Printstagram</h1>
@@ -74,7 +124,7 @@ export default function ProjectPage() {
             >
               Live
             </a>
-            <h3>React, CSS, Supabase, Jest, Netlify.</h3>
+            <h3>React, Supabase, Jest.</h3>
             <p>
               A mobile-first Instagram clone powered by the Petfinder API that acts as social media
               for animals that are currently up for adoption.
@@ -96,6 +146,7 @@ export default function ProjectPage() {
             </div>
           </div>
         </div>
+
         <div className="project-card" id="vsssnake">
           <div className="content">
             <h1>VSsSnake</h1>
@@ -115,7 +166,7 @@ export default function ProjectPage() {
             >
               Live
             </a>
-            <h3>HTML, CSS, JavaScript, Supabase, Netlify.</h3>
+            <h3>HTML, CSS, JavaScript, Supabase.</h3>
             <p>
               A fun and nostalgic arcade-style game inspired by Nokia Snake that assists software
               engineering students as they memorize the keyboard shortcuts to navigate VS Code.
@@ -132,11 +183,13 @@ export default function ProjectPage() {
               <a href="#printstagram">
                 <img className="arrow" src="/arrowup.png" />
               </a>
+              <a href="#top">
+                <img id="top-arrow" className="arrow" src="/toparrow.png" />
+              </a>
             </div>
           </div>
         </div>
       </div>
-      {/* <footer><a href="/connect">Connect</a></footer> */}
     </div>
   );
 }
